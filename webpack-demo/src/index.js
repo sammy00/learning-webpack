1180b42f0374b11cfb933990228cbfac50143b23
sammy00 <xiangminli@sjtu.edu.cn>
2018-05-20 13:21:54 +0800

    done enabling HMR

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -18,3 +18,10 @@ function component() {
 }
 
 document.body.appendChild(component());
+
+if (module.hot) {
+  module.hot.accept('./print.js', () => {
+    console.log('Accepting the updated printMe module!');
+    printMe();
+  });
+}
