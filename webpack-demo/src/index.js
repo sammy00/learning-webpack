7d70baa695333e3a7e9c8cddd0e764bd4e44cc89
sammy00 <xiangminli@sjtu.edu.cn>
2018-05-20 12:06:47 +0800

    backup for wrapping up

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,12 +1,24 @@
 'use strict';
 
 import _ from 'lodash';
+import './style.css';
+import Icon from './icon.svg';
+import Data from './data.xml';
 
 function component() {
   const element = document.createElement('div');
   //- Lodash, currently included via a script, is required for this line to work
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  const myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
+
+  console.log(Data);
 
   return element;
 }
