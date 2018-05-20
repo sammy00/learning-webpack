6d59d336123113879cb259259f6de8a26ee973b2
sammy00 <xiangminli@sjtu.edu.cn>
2018-05-20 12:44:35 +0800

    done output management

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,24 +1,18 @@
 'use strict';
 
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.svg';
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
-  //- Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
+  const btn = document.createElement('button');
+
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  element.appendChild(myIcon);
-
-  console.log(Data);
+  element.appendChild(btn);
 
   return element;
 }
